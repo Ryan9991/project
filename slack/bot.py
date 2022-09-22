a2866ba56b290e5f31595c05f18e6e61dc8944ce
ryan.adi <ryan.adi@sirclo.com>
2022-09-22 14:44:47 +0700

    testing for airflow

--- slack/bot.py
+++ slack/bot.py
@@ -15,138 +15,14 @@ slack_event_adapter = SlackEventAdapter(
 client = slack.WebClient(token = os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
-true = True
 
 @slack_event_adapter.on('message')
 def message(payload):
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user = event.get('user')
-    text = event.get('text')
     if BOT_ID != user:
-        client.chat_postMessage(channel=channel_id, blocks = [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": f"You have a new request:\n*<fakeLink.toEmployeeProfile.com|{user} - New device request>*"
-			}
-		},
-		{
-			"type": "section",
-			"fields": [
-				{
-					"type": "mrkdwn",
-					"text": "*Type:*\nComputer (laptop)"
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*When:*\nSubmitted Aut 10"
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*Last Update:*\nMar 10, 2015 (3 years, 5 months)"
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*Reason:*\nAll vowel keys aren't working."
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\""
-				}
-			]
-		},
-		{
-			"type": "actions",
-			"elements": [
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"emoji": true,
-						"text": "Approve"
-					},
-					"style": "primary",
-					"value": "click_me_123"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"emoji": true,
-						"text": "Deny"
-					},
-					"style": "danger",
-					"value": "click_me_123"
-				}
-			]
-		}
-	])
-
-@app.route('/interactivity', methods=['POST'])
-def response():
-    print(request.values['payload'])
-    client.chat_postMessage(channel='#testing-ryan', blocks = [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": f"You have a new request:\n*<fakeLink.toEmployeeProfile.com| - New device request>*"
-			}
-		},
-		{
-			"type": "section",
-			"fields": [
-				{
-					"type": "mrkdwn",
-					"text": "*Type:*\nComputer (laptop)"
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*When:*\nSubmitted Aut 10"
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*Last Update:*\nMar 10, 2015 (3 years, 5 months)"
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*Reason:*\nAll vowel keys aren't working."
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\""
-				}
-			]
-		},
-		{
-			"type": "actions",
-			"elements": [
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"emoji": true,
-						"text": "Approve"
-					},
-					"style": "primary",
-					"value": "click_me_123"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"emoji": true,
-						"text": "Deny"
-					},
-					"style": "danger",
-					"value": "click_me_123"
-				}
-			]
-		}
-	])
-    return Response(), 200
+        print(event)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True, port = 3000)
